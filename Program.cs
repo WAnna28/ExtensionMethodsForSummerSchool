23b2d3d49d420e4a1745c23fba9e78ce2afcafa4
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2021-09-29 04:49:21 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ExtensionMethodsForSummerSchool.Extensions;
+using System;
+
+// .NET 3.5 introduced the concept of extension methods, which allow you to add new methods or properties
+// to a class or structure, without modifying the original type in any direct manner.
+namespace ExtensionMethodsForSummerSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+            
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            Console.WriteLine($"Value of myInt: {myInt}");
+            Console.WriteLine($"Reversed digits of myInt: {myInt.ReverseDigits()}");
+
+            Console.ReadLine();
+        }
+    }
+}
